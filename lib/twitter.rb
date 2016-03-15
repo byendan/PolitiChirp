98e97e1469900c3f6f716888893e675e6a836c2d
byendan <byendanb@gmail.com>
2016-03-14 23:42:16 -0700

    Better styles

--- lib/twitter.rb
+++ lib/twitter.rb
@@ -1,30 +1,30 @@
 def fill_pundits(pundits)
   handles = pundits.collect {|p| p.twitter_handle }.join ','
-  
+
   baseurl = "https://api.twitter.com"
   path    = "/1.1/users/lookup.json"
   query   = "#{handles}"
   address = URI("#{baseurl}#{path}?screen_name=#{query}")
   request = Net::HTTP::Get.new address.request_uri
-  
+
   # Set up HTTP.
   http             = Net::HTTP.new address.host, address.port
   http.use_ssl     = true
   http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-  
+
   consumer_key = OAuth::Consumer.new(
     ENV["twitter_consumer_key"],
     ENV["twitter_consumer_secret"])
   access_token = OAuth::Token.new(
     ENV["twitter_access_token"],
     ENV["twitter_access_secret"])
-    
+
   # Issue the request.
   request.oauth! http, consumer_key, access_token
   http.start
   response = http.request request
-  
-  
+
+
   # Parse and print the Tweet if the response code was 200
   tweet = nil
   if response.code == '200' then
@@ -43,6 +43,7 @@ private
         screen_name:  tweet["screen_name"],
         avatar:       tweet["profile_image_url"],
         last_tweet:   tweet["status"]["text"],
+        tweet_time:   tweet["status"]["created_at"],
         link:         tweet["url"]
       }
       pundits.push(pundit)
